1b22588f0ac4efc0023e31c9c148e96145dc75e8
Kartik Sharma <kartik19952012@gmail.com>
2021-07-23 01:59:29 +0530

    changes nginx log files path

--- config/settings/base.py
+++ config/settings/base.py
@@ -15,21 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-34elbjy)3pa=%#$r5d=+s1@quvsz@crn!+&vx=n=639(03b-0h'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['127.0.0.1']
-
-
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
---
+++ config/settings/debug.py
@@ -0,0 +1,12 @@
+# Copy this file to local.py if running in local
+from .base import *
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-34elbjy)3pa=%#$r5d=+s1@quvsz@crn!+&vx=n=639(03b-0h'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+ALLOWED_HOSTS = ['*']
